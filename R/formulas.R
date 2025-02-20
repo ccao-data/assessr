a1d7a7586fdea35474bdbb9fa33e58a4d06e028d
Damonamajor <56321109+Damonamajor@users.noreply.github.com>
2025-02-20 15:43:55 -0600

    Update R/formulas.R
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>

--- R/formulas.R
+++ R/formulas.R
@@ -320,7 +320,7 @@ prb_met <- function(x) x >= -0.05 & x <= 0.05
 #' @export
 mki_met <- function(x) x >= 0.95 & x <= 1.05
 
-#' @describeIn med_ratio_met Returns TRUE when input meets IAAO standards
+#' @describeIn med_ratio_met Returns TRUE when input sales ratio meets IAAO standards
 #'   (between 0.9 and 1.1).
 #' @inheritParams cod_met
 #' @export
