98116566f955b6e2343cd74c9826d7d4611c058b
Damonamajor <56321109+Damonamajor@users.noreply.github.com>
2025-09-03 15:09:29 -0500

    rename test

--- tests/testthat/test-formulas.R
+++ tests/testthat/test-formulas.R
@@ -193,7 +193,7 @@ test_that("standard met function", {
   expect_false(mki_met(mki_out))
 })
 
-test_that("all estimate variants return the same MKI (tiebreak data)", {
+test_that("all sorting variations return the same MKI (tiebreak data)", {
   mki_out_assessed <- mki(
     tiebreaks_Sale_Price,
     tiebreaks_Assessed
@@ -250,7 +250,7 @@ test_that("incomplete data returns NAs unless removed", {
   )
 })
 
-test_that("all estimate variants return the same ki (tiebreak data)", {
+test_that("all sorting variations return return the same ki (tiebreak data)", {
   ki_out_assessed <- ki(
     tiebreaks_Sale_Price,
     tiebreaks_Assessed
